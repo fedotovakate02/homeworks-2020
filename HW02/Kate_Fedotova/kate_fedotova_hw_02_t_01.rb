bf5c218df2b24462df4464a5b7b5fc912b8abcc6
Kate Fedotova <you@example.com>
2020-03-21 14:27:59 +0300

    task_01_inplementation

---
+++ HW02/Kate_Fedotova/kate_fedotova_hw_02_t_01.rb
@@ -0,0 +1,19 @@
+log_file = File.open('task_1.txt'){|file| file.read}
+
+unless File.exist?('task_1.txt')
+    abort 'File is not exist!'
+end
+ 
+if File.open('task_1.txt')
+puts 'File open:' + 'task_1.txt'
+end
+
+file = File.new('task_1.txt')
+
+lines = file.readlines
+
+lines.each do |line|
+    if line.include? 'error'
+        puts line
+    end
+end
