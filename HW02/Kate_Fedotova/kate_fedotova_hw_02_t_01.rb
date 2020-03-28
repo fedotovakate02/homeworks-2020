5da67d12725d29202bea9976ee3fd80a9bd5ea5d
Kate Fedotova <you@example.com>
2020-03-28 15:02:45 +0300

    fix review comments

--- HW02/Kate_Fedotova/kate_fedotova_hw_02_t_01.rb
+++ HW02/Kate_Fedotova/kate_fedotova_hw_02_t_01.rb
@@ -1,9 +1,11 @@
-puts 'File open: task_1.txt' if File.open('task_1.txt')
-
 abort 'File is not exist!' unless File.exist?('task_1.txt')
 
 lines = File.readlines('task_1.txt')
 
-lines.each do |line|
-  puts line if line.include?('error')
+def task_1(logs)
+  logs.each do |line|
+    return line if line.include?('error')
+  end
 end
+
+puts task_1(lines)
