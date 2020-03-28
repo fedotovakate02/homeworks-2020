cb9ec5611e3e98cf47225779d4080316b80fb8ae
Kate Fedotova <you@example.com>
2020-03-28 19:58:08 +0300

    fix comments

--- HW02/Kate_Fedotova/kate_fedotova_hw_02_t_03.rb
+++ HW02/Kate_Fedotova/kate_fedotova_hw_02_t_03.rb
@@ -1,22 +1,18 @@
 require 'time'
 
-puts 'File open: task_2.txt' if File.open('task_2.txt')
-
 abort 'File is not exist!' unless File.exist?('task_2.txt')
 
 logs = File.readlines('task_3.txt')
 
 def get_events_interval(lines)
-  array = []
   valid_events = lines.select { |line| line.include?('Calling core with') }
   return [0] if valid_events.size <= 1
 
   valid_events.each_cons(2).map do |first, second|
     first_log = first.match(/^\d*\-\d.\-\d. \d.\:\d.\:\d.\.\d/)[0]
     second_log = second.match(/^\d*\-\d.\-\d. \d.\:\d.\:\d.\.\d/)[0]
-    array.push Time.parse(second_log) - Time.parse(first_log)
+    Time.parse(second_log) - Time.parse(first_log)
   end
-  array
 end
 
 puts get_events_interval(logs)
