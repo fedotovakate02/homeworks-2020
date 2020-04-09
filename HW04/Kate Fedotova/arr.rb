f833d6fd639fc30278d4569d5521f851a45aaf70
Kate Fedotova <you@example.com>
2020-04-09 15:15:00 +0300

    task implementation

---
+++ HW04/Kate Fedotova/arr.rb
@@ -0,0 +1,34 @@
+class Array
+  def my_each
+    return self unless block_given?
+
+    (0..length - 1).each do |item_idx|
+      yield(self[item_idx])
+    end
+
+    self
+  end
+
+  def my_map
+    return clone.to_enum unless block_given?
+
+    result = []
+    (0..length - 1).each do |item_idx|
+      result << yield(self[item_idx])
+    end
+
+    result
+  end
+
+  def my_select
+    return to_enum unless block_given?
+
+    result = []
+    i = 0
+    (0..length - 1).each do |item_idx|
+      result << self[item_idx] if yield(self[item_idx])
+    end
+
+    result
+      end
+end
