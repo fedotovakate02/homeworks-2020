e8a7d424712738c8b3fc57ff9f6d800a41cd1434
Kate Fedotova <you@example.com>
2020-04-29 18:46:45 +0300

    fix comments

--- HW04/Kate Fedotova/array_spec.rb
+++ HW04/Kate Fedotova/array_spec.rb
@@ -1,39 +1,43 @@
 require 'rspec'
 require 'rubocop-rspec'
 require_relative 'arr.rb'
-
 # rubocop:disable Metrics/BlockLength
 describe Array do
-  describe 'my_each' do
-    let(:arr) { [1, 2, 3, 4] }
+  describe '#my_each' do
+    subject(:arr) { [1, 2, 3, 4] }
 
-    it 'has block' do
+    context 'when has block' do
+      it 'return arr items +1'
       new_arr = []
       arr.my_select { |el| new_arr << el }
       expect(new_arr).to eq([1, 2, 3, 4])
     end
   end
 
-  describe 'my_map' do
-    let(:arr) { [1, 2, 3, 4] }
+  describe '#my_map' do
+    subject(:arr) { [1, 2, 3, 4] }
 
-    it 'has block' do
+    context 'when has block' do
+      it 'return arr items +1'
       expect(arr.my_map { |el| el + 1 }).to eq([2, 3, 4, 5])
     end
 
-    it 'without block' do
+    context 'when without block' do
+      it 'return arr items +1'
       expect(arr.my_select).to be_an_instance_of(Enumerator)
     end
   end
 
-  describe 'my_select' do
-    let(:arr) { [1, 2, 3, 4] }
+  describe '#my_select' do
+    subject(:arr) { [1, 2, 3, 4] }
 
-    it 'has block' do
+    context 'when has block' do
+      it 'return arr items +1'
       expect(arr.my_select { |el| el >= 2 }).to eq([2, 3, 4])
     end
 
-    it 'without block' do
+    context 'when without block' do
+      it 'return arr items +1'
       expect(arr.my_select).to be_an_instance_of(Enumerator)
     end
   end
